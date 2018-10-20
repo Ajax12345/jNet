9423932407d398273d4b65bf0c2fac7918b3fc38
James Petullo <pentester18@gmail.com>
2018-10-20 14:05:12 -0400

    Update jnet_server_connector.py

--- jnet_server_connector.py
+++ jnet_server_connector.py
@@ -16,5 +16,5 @@ def request_0(_payload:dict) -> typing.Dict[str, bool]:
 def request_1(_payload:dict) -> dict:
     _app_inst = importlib.import_module(f'apps.app_{_payload["app"]}.app_routes').app
     _framework_results = _app_inst.build(_payload['path'], _sessions = _payload['session'], **_payload['forms'])
-    return {'route':_framework_results.route,'is_redirect':_framework_results.isredirect, 'session':_app_inst.sessions.session, 'response-type':[1, 2][_framework_results.content == 'env'], 'payload':dict(_framework_results)}
+    return {'route':_framework_results.route,'is_redirect':_framework_results.isredirect, 'session':_app_inst.sessions.session, 'response-type':[1, 2][_framework_results.content == 'env'], 'payload':dict(_framework_results.content)}
 
