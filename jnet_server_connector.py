42fd991f579ec6adbc1b7b3286ccc7bbc81d1440
James Petullo <pentester18@gmail.com>
2018-10-20 13:43:22 -0400

    Update jnet_server_connector.py

--- jnet_server_connector.py
+++ jnet_server_connector.py
@@ -1,4 +1,4 @@
-import os, re, typing
+import os, re, typing, importlib
 import json, jnet_utilities, pickle
 
 def matching_app(_folder:str, _name:str, live=True) -> bool:
@@ -14,8 +14,7 @@ def request_0(_payload:dict) -> typing.Dict[str, bool]:
 
 @jnet_utilities.log_view
 def request_1(_payload:dict) -> dict:
-    import apps
-    _app_inst = getattr(apps, f'app_{_payload["app"]}').app_routes.app
+    _app_inst = importlib.import_module(f'apps.app_{_payload["app"]}.app_routes').app
     _framework_results = _app_inst.build(_payload['path'], _sessions = _payload['session'], **_payload['forms'])
     return {'route':_framework_results.route,'is_redirect':_framework_results.isredirect, 'session':_app_inst.sessions.session, 'response-type':[1, 2][_framework_results.content == 'env'], 'payload':dict(_framework_results)}
 
