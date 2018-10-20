fa7f340ceadc5b0efb68d8fa8a9f0ea70c3bdf0b
James Petullo <pentester18@gmail.com>
2018-10-19 21:34:46 -0400

    Update on_connection_actions.py

--- on_connection_actions.py
+++ on_connection_actions.py
@@ -9,5 +9,5 @@ def on_connection(c, addr) -> dict:
     c.sendall(pickle.dumps({'echo':pickle.loads(_data)}))
     '''
     payload = {**{'ip':addr[0]}, **pickle.loads(c.recv(1024))}
-    _new_payload = getattr(jnet_server_connector, f'request_{payload["request-type"]}')(payload['payload'])
+    _new_payload = getattr(jnet_server_connector, f'request_{payload["request-type"]}')({**payload, **payload['payload']})
     return c.sendall(pickle.dumps(_new_payload))
